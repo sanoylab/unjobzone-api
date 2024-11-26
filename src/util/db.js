75713ebae6f83c54de232641b3356ea09ad66894
yonas <expertsanoy@gmail.com>
2024-11-26 09:03:46 -0700

    fix ssl issue

--- src/util/db.js
+++ src/util/db.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const credentials = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  ssl:false
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 };
 
 
